18790ee2cff29fbf1b5b5684cfb89cc53cf84344
Alyffantonio <146452808+Alyffantonio@users.noreply.github.com>
2024-07-26 22:41:40 -0300

    Delete CardapioBack directory

--- CardapioBack/src/main/java/com/Cardapio/Food/FoodRepository.java
+++
@@ -1,6 +0,0 @@
-package com.Cardapio.Food;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {
-}
--- CardapioBack/src/main/java/com/Cardapio/Food/FoodRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package com.Cardapio.Food;
-
-public record FoodRequestDTO(String title, String image, Integer price) {
-}
